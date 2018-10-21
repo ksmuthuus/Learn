381b6ac669f2aa34b492d93d4fc76153cf121105
Muthu Venkatachalam <muthukarupasamy.venkatachalam@honeywell.com>
2018-10-21 07:15:51 +0530

    added coaching

---
+++ Coaching/LINQ/Manufacturer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Coaching.LINQ
+{
+    public class Manufacturer
+    {
+        public string Name { get; set; }
+        public string HeadQuarters { get; set; }
+        public int Year { get; set; }
+
+        public IEnumerable<Manufacturer> ProcessManufacturers(string path)
+        {
+            var query = 
+                File.ReadAllLines(path)
+                .Where(line => line.Length > 1)
+                .Select(line =>
+                {
+                    var columns = line.Split(",");
+                    return new Manufacturer
+                    {
+                        Name = columns[0],
+                        HeadQuarters = columns[1],
+                        Year = int.Parse(columns[2])
+                    };
+                });
+
+            return query.ToList();
+        }
+    }
+}
---
+++ Coaching/LINQ/Program.cs
@@ -0,0 +1,19 @@
+﻿using Coaching.LINQ;
+using System;
+
+namespace Coaching
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Manufacturer m = new Manufacturer();
+            var result = m.ProcessManufacturers("manufacturers.csv");
+
+            foreach(var data in result)
+            {
+                Console.WriteLine(data.HeadQuarters);
+            }
+        }
+    }
+}
