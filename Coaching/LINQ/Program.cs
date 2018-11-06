b585dbe51b8d9f10b738fa525c8d7dd0ec72deb7
Muthu Venkatachalam <muthukarupasamy.venkatachalam@honeywell.com>
2018-11-06 13:09:07 +0530

    New projects added

--- Coaching/LINQ/Program.cs
+++ Coaching/LINQ/Program.cs
@@ -14,7 +14,7 @@ namespace Coaching
 
             Manufacturer m = new Manufacturer();
             var manufacturers = m.ProcessManufacturers("manufacturers.csv");
-
+            #region joins
             var query = from car in cars
                         join manufacturer in manufacturers
                         on car.Manufacturer equals manufacturer.Name
@@ -38,10 +38,25 @@ namespace Coaching
                                     })
                                     .OrderByDescending(car => car.Combined)
                                     .ThenBy(car => car.Name);
+            #endregion
 
-            foreach(var result in query1.Take(10))
+            #region Group
+            var query2 = from car in cars
+                         group car by car.Manufacturer.ToUpper() into manufacturer
+                         orderby manufacturer.Key
+                         select manufacturer;
+
+            var query3 = cars.GroupBy(car => car.Manufacturer.ToUpper())
+                .OrderBy(g => g.Key);
+            #endregion
+
+            foreach(var result in query3)
             {
-                Console.WriteLine($"{result.HeadQuarters} {result.Name} {result.Combined}");
+                Console.WriteLine($"{result.Key} Count: {result.Count()}");
+                foreach(var car in result.OrderByDescending(car => car.Combined).Take(2))
+                {
+                    Console.WriteLine($"\t{car.Name}:{car.Combined}");
+                }
             }
         }
     }
